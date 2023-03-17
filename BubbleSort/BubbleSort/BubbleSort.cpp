28a8ab3493e84eed37f499783f4fd60fc76bfcf2
Mashell0707 <merlsutama@gmail.com>
2023-03-17 18:30:45 +0700

    prosedur untuk menampilkan hasil

--- BubbleSort/BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort/BubbleSort.cpp
@@ -15,6 +15,18 @@ void input() {
 		}
 	}
 	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
+}
+
+void display() {
+	cout << endl;
 	cout << "=================================" << endl;
 	cout << "Element Array yang telah tersusun" << endl;
 	cout << "=================================" << endl;
@@ -23,3 +35,4 @@ void input() {
 	}
 	cout << endl;
 }
+
