eb41fcd2a171f2de7fb8cab3b2bb0e43fbacd41e
Mashell0707 <merlsutama@gmail.com>
2023-03-17 18:29:45 +0700

    menambahkan prosedur untuk input elemen pada array

--- BubbleSort/BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort/BubbleSort.cpp
@@ -4,3 +4,22 @@ using namespace std;
 int a[20];
 int n;
 
+void input() {
+	while (true) {
+		cout << "masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+	}
+	cout << endl;
+}
