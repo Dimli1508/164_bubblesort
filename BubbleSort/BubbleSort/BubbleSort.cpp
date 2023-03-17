1d682ed18a92fc3a612ae70515789c3c7907650c
Mashell0707 <merlsutama@gmail.com>
2023-03-17 18:36:20 +0700

    prosedur untuk mengurutkan array dengan metode bubble sort

--- BubbleSort/BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort/BubbleSort.cpp
@@ -36,3 +36,21 @@ void display() {
 	cout << endl;
 }
 
+void bubbleSortArray() {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - i; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp = a[j];
+				a[j]] = a[j];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	input();
+	bubbleSortArray();
+	display();
+	return 0;
+}
